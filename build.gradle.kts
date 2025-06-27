1474a85e0597dfc265949de7af069970d5db78e6
Hogyu,Kim <hgkimer@gmail.com>
2025-06-27 20:29:16 +0900

    CH05-02 Spring Boot 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id ("org.springframework.boot") version "3.5.3"
+    // dependency-management: 스프링과 다른 의존관계들의 버전을 관리해 준다
+    id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "org.fastcampus"
@@ -10,8 +13,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
--- src/main/java/org/fastcampus/community_feed/user/application/UserService.java
+++ src/main/java/org/fastcampus/community_feed/user/application/UserService.java
@@ -4,19 +4,20 @@ import org.fastcampus.community_feed.user.application.dto.CreateUserRequestDto;
 import org.fastcampus.community_feed.user.application.interfaces.UserRepository;
 import org.fastcampus.community_feed.user.domain.User;
 import org.fastcampus.community_feed.user.domain.UserInfo;
+import org.springframework.stereotype.Service;
 
+//@Service
 // 비즈니스 로직을 처리하기 보다는 다른 객체와 협업하는 객체
 public class UserService {
 
   /**
-   * 저장 기능을 위해 Repository 레이어에 접근 필요
-   * But Repository와 같은 저수준 컴포넌트는 비즈니스 로직을 처리하는 도메인이나 어플리케이션 레이어에서
-   * 직접 의존하면 안된다고 지난 시간에 배움(클린 아키텍쳐 부분).
-   * => 따라서 Repository에 접근할 수 있는 인터페이스를 만들어 의존성을 역전시키도록 함
+   * 저장 기능을 위해 Repository 레이어에 접근 필요 But Repository와 같은 저수준 컴포넌트는 비즈니스 로직을 처리하는 도메인이나 어플리케이션 레이어에서
+   * 직접 의존하면 안된다고 지난 시간에 배움(클린 아키텍쳐 부분). => 따라서 Repository에 접근할 수 있는 인터페이스를 만들어 의존성을 역전시키도록 함
    */
 
   private final UserRepository userRepository;
-
+  // Spring 적용 + @Service 추가 후, UserRepository를 주입하려고 했으나
+  // 인터페이스를 구현한 구현체를 찾지 못해 에러 표시 됨.
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
